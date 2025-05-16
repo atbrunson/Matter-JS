abd93c3969e122f459b0dad596b5f516dbbdcb0e
PressureSwitch <atbrunson@gmail.com>
2025-05-15 22:31:48 -0500

    started Player class

--- player_class/lib/player.js
+++ player_class/lib/player.js
@@ -1,11 +1,28 @@
+/** Player Class
+ * The @class Player class creates a new controllable
+ * object to interact within a predefined engine.world
+ * 
+ * The Player object needs at least an origin & a height:
+ * @param x of origin
+ * @param y of origin
+ * @param height of Player object
+*/
 class Player {
-	constructor(x, y, width, height, options = {}) {
-		this.body = Matter.Bodies.rectangle(x, y, width, height, { ...options });
-		Matter.World.add(engine.world, this.body);
+	constructor(x, y, height, width = height/2,  options = {}) {
+		
+		this.body = Matter.Bodies.rectangle(x, y, width, height, {
+			chamfer: { radius: width/2.2 }
+			}
+		);
 		this.width = width;
 		this.height = height;
+		Matter.World.add(engine.world, this.body);
 	}
 
+	follow(){
+		// TODO: implement keeping the Player
+		 // object centered in the viewPort 
+	}
 	moveLeft(speed) {
 		Matter.Body.setVelocity(this.body, { x: -speed, y: this.body.velocity.y });
 	}
@@ -20,14 +37,7 @@ class Player {
 
 	update() {
 		// Additional logic to update player state
+
 	}
 
-	render(ctx) {
-		const { x, y } = this.body.position;
-		ctx.beginPath();
-		ctx.rect(x - this.width / 2, y - this.height / 2, this.width, this.height);
-		ctx.fillStyle = 'blue';
-		ctx.fill();
-		ctx.closePath();
-	}
 }
--- player_class/main.js
+++ player_class/main.js
@@ -1,8 +1,8 @@
-//matter.js testing
 
+// Matter.jd Module Aliases
 
-// module aliases
-const Engine = Matter.Engine,
+const
+	Engine = Matter.Engine,
 	Render = Matter.Render,
 	Runner = Matter.Runner,
 	Body = Matter.Body,
@@ -13,14 +13,21 @@ const Engine = Matter.Engine,
 	Mouse = Matter.Mouse,
 	Bodies = Matter.Bodies;
 
-// create engine
+// Create the ENGINE & the WORLD
 let engine = Engine.create(),
 	world = engine.world;
 
-// set world properties
-	world.gravity.scale = 0.0
+// Set WORLD Properties
+world.gravity.scale = 0.0
 
-// create renderer
+// Create the RUNNER
+let runner = Runner.create();
+
+// Run the ENGINE
+Runner.run(runner, engine);
+
+
+// Create RENDERER
 let render = Render.create({
 	element: document.body,
 	engine: engine,
@@ -35,20 +42,15 @@ let render = Render.create({
 	}
 });
 
-// fit the render viewport to the scene
+// Run the RENDERER
+Render.run(render);
+
+// Arrange the VIEWPORT in the world
+// TODO: implement zoom & edge scrolling
 Render.lookAt(render, {
 	min: { x: 0, y: 0 },
 	max: { x: 800, y: 600 }
 });
-
-// run the renderer
-Render.run(render);
-
-// create runner
-let runner = Runner.create();
-
-// run the engine
-Runner.run(runner, engine);
 
 // add mouse control
 let mouse = Mouse.create(render.canvas),
@@ -74,35 +76,29 @@ let ground = Bodies.rectangle(400, 625, 1000, 60, {
 	isStatic: true,
 	restitution: 1,
 
-	});
+});
 let leftWall = Bodies.rectangle(-25, 300, 60, 1000, {
 	isStatic: true,
 	restitution: 1,
 
-	});
+});
 
 let rightWall = Bodies.rectangle(825, 300, 60, 1000, {
 	isStatic: true,
 	restitution: 1,
 
-	});
+});
 
 let ceiling = Bodies.rectangle(400, -25, 1000, 60, {
 	isStatic: true,
 	restitution: 1,
 
-	});
+});
 
 Composite.add(box, ground)
 Composite.add(box, leftWall)
 Composite.add(box, rightWall)
 Composite.add(box, ceiling)
-
-let linkDia = 30;
-let linkMass = 10;
-let cLength = 10;
-let cDamp = 1
-let cStiff = .1
 
 
 // generate ring of bodies
@@ -110,60 +106,16 @@ let cStiff = .1
 // - create function that generates a ring of bodies
 // - ringChain(, ringDia{default => no collisions}) circumference = bodies
 
-let flinks = Composites.stack(50, 175, 10, 1, 5, 5 , function(x, y) {
-	return Bodies.circle(x , y , linkDia, {
-			render: {fillStyle: "darkgrey"},
-			mass: linkMass,
-			dampening: cDamp,
-	
-			})
-		});
+let player1 = new Player(250, 250, 100)
 
 
-Composites.chain(flinks, 0.45 , 0, -0.45, 0, {
-	stiffness: cStiff,
-	length: cLength,
-	mass: linkMass,
-	render: {
-		type: 'spring', 
-		visible: true,
-		},
 
-	})
-
-/*
-
-Add a constraint from the last chain body to the first chain body
-TODO:
-- should add closing constraint in the same way that the Composites.chain() does
-- create function to close a chained composite
-> function closeLoop(chain)
-
-*/
-
-Composite.add(flinks, Constraint.create({
-	bodyA: flinks.bodies[flinks.bodies.length - 1],
-	pointA: { x: 30, y: 0},
-	bodyB: flinks.bodies[0],
-	pointB: { x: -30, y: 0 },
-	stiffness: cStiff,
-	length: cLength,
-	
-	render: {
-		type: 'spring',
-		visible: true,
-	},
-	}));
-
-flinks.bodies[0].render.fillStyle = "Red"
-flinks.bodies[flinks.bodies.length-1].render.fillStyle = "Blue"
 
 
 
 Composite.add(world,
-	[box, flinks]
+	[box]
 );
-
 
 
 
