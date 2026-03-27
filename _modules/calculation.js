1ff6f828e7669b10712c361ae85dc33bc85496da
Andrew Brunson <atbrunson@gmail.com>
2026-03-27 15:50:46 -0500

    Clean up & Ogranize

---
+++ _modules/calculation.js
@@ -0,0 +1,26 @@
+/**
+ * @module Calculation
+ * @description try's to process the variable and regular expression before each Matter.js update 
+ * @example this.update = new Calculation{
+ * 
+ * } 
+ */
+
+
+class Calculation{
+	constructor( {variables}, regex) {
+
+		
+		for (v in variables){
+			temp = v => regex
+		}
+
+
+
+
+		// add event watcher and execute "beforeUpdate"
+		// add method to perform calculation
+	}
+
+
+}
